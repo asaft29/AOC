b0a109a730a93019b390ce19356c08b4e477e3ca
Asaftei Rares <rares-stefan.asaftei@student.tuiasi.ro>
2025-05-16 17:34:04 +0300

    oops

--- 2024/day_02/src/part_1.rs
+++ 2024/day_02/src/part_1.rs
@@ -1,60 +1,56 @@
 use std::{cmp::Ordering, error::Error, fs::read_to_string};
 
-
-fn check_ascending(vec : &Vec<i32>) -> bool
-{
+fn check_ascending(vec: &Vec<i32>) -> bool {
     let mut iter = vec.iter();
 
-    if let Some(mut value) = iter.next()
-    {
-        for item in iter
-        {
-            if item - value <= 3 && item - value >= 1
-            {
+    if let Some(mut value) = iter.next() {
+        for item in iter {
+            if item - value <= 3 && item - value >= 1 {
                 value = item;
-            }
-            else {
+            } else {
                 return false;
             }
         }
     }
-   true
+    true
 }
 
-fn check_descending(vec : &Vec<i32>) -> bool
-{
+fn check_descending(vec: &Vec<i32>) -> bool {
     let mut iter = vec.iter();
 
-    if let Some(mut value) = iter.next()
-    {
-        for item in iter 
-        {
-            if value - item <= 3 && value - item >= 1
-            {
+    if let Some(mut value) = iter.next() {
+        for item in iter {
+            if value - item <= 3 && value - item >= 1 {
                 value = item;
-            }
-            else {
+            } else {
                 return false;
             }
         }
     }
-   true
+    true
 }
 
-fn count_reports(file : &str) -> Result<i32, Box<dyn Error>>
-{
+fn count_reports(file: &str) -> Result<i32, Box<dyn Error>> {
     let mut safe = 0;
     let inputs = read_to_string(file)?;
-    for line in inputs.lines()
-    {
-        let vec = line.split_whitespace().filter_map(|val| val.parse::<i32>().ok()).collect::<Vec<i32>>();
+    for line in inputs.lines() {
+        let vec = line
+            .split_whitespace()
+            .filter_map(|val| val.parse::<i32>().ok())
+            .collect::<Vec<i32>>();
         let mut iter = vec.iter();
-        if let (Some(first), Some(second)) = (iter.next(), iter.next())
-        {
-            match first.cmp(&second)
-            {
-                Ordering::Less => { if check_ascending(&vec) { safe += 1 } },
-                Ordering::Greater => { if check_descending(&vec) { safe += 1} },
+        if let (Some(first), Some(second)) = (iter.next(), iter.next()) {
+            match first.cmp(&second) {
+                Ordering::Less => {
+                    if check_ascending(&vec) {
+                        safe += 1
+                    }
+                }
+                Ordering::Greater => {
+                    if check_descending(&vec) {
+                        safe += 1
+                    }
+                }
                 Ordering::Equal => (),
             }
         }
@@ -62,8 +58,7 @@ fn count_reports(file : &str) -> Result<i32, Box<dyn Error>>
     Ok(safe)
 }
 
-pub fn execute() -> Result<(), Box<dyn Error>>
-{
+pub fn execute() -> Result<(), Box<dyn Error>> {
     let reports = count_reports("input.txt")?;
     println!("Part 1 - {reports}");
     Ok(())
