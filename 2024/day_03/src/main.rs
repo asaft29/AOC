1da02a64151daa44ffae48047f46022f95a2aece
Asaftei Rares <rares-stefan.asaftei@student.tuiasi.ro>
2025-05-17 12:34:18 +0300

    day 3 - part 1

---
+++ 2024/day_03/src/main.rs
@@ -0,0 +1,7 @@
+mod part_1;
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+
+    part_1::execute()?;
+    Ok(())
+}
---
+++ 2024/day_03/src/part_1.rs
@@ -0,0 +1,27 @@
+use std::{error::Error, fs::read_to_string};
+use regex::Regex;
+
+
+fn find_numbers(path : &str) -> Result<u64, Box<dyn Error>>
+{
+    let mut result : u64 = 0;
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)")?;
+    let inputs = read_to_string(path)?;
+    for pair in regex.captures_iter(&inputs)
+    {
+        
+        let first = pair[1].parse::<u64>()?;
+        let second = pair[2].parse::<u64>()?;
+        result += first * second;
+    }
+    Ok(result)
+}
+
+
+pub fn execute() -> Result<(), Box<dyn Error>> 
+{
+    let res = find_numbers("input.txt")?;
+    println!("Part 1 - {res}");
+    Ok(())
+
+}
